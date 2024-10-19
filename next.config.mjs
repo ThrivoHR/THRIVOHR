18b7a0df3b6889f342cad84cd5c5de425fc56594
DatPham1103 <phamtdat1103@gmail.com>
2024-10-19 21:22:53 +0700

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,38 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-      remotePatterns: [
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes
+        source: "/(.*)",
+        headers: [
           {
-            protocol: "https",
-            hostname: "**",
+            key: "Content-Security-Policy",
+            value: "default-src 'self'; media-src 'self' blob:;",
           },
-         
-      ],
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin",
+          },
+          {
+            key: "Cross-Origin-Embedder-Policy",
+            value: "require-corp",
+          },
+          {
+            key: "Permissions-Policy",
+            value: "camera=(self)",
+          },
+        ],
+      },
+    ];
   },
 };
 
