2f24f89eaffaa33ec4621704b7ec0d1507b390c0
DatPham1103 <phamtdat1103@gmail.com>
2024-10-25 14:45:52 +0700

    small fix

--- src/app/home/attendence/checkInOut/columns.tsx
+++ src/app/home/attendence/checkInOut/columns.tsx
@@ -16,10 +16,12 @@ export const columns = (
   {
     accessorKey: "checkIn",
     header: "Check-In",
+    cell: ({ row }) => dayjs(row.original.checkIn).format('hh/mm/ss'),
   },
   {
     accessorKey: "checkOut",
     header: "Check-Out",
+    cell: ({ row }) => dayjs(row.original.checkOut).format('hh/mm/ss'),
   },
   {
     accessorKey: "note",
